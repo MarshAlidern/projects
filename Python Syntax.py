ae3b5ba1fead3f8c2edf20e0225d120a7072d53a
MarshAlidern <136602503+MarshAlidern@users.noreply.github.com>
2026-01-30 16:48:18 +0500

    Update and rename test2.py to Python Syntax.py

---
+++ Python Syntax.py
@@ -0,0 +1,10 @@
+a=1
+b=2
+print(a+b+a*b)
+print(a-b*(a+a+b))
+print("Have you eaten your vegies?")
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+#This is a comment.
--- test2.py
+++
@@ -1,5 +0,0 @@
-a=1
-b=2
-print(a+b+a*b)
-print(a-b*(a+a+b))
-print("Have you eaten your vegies?")
