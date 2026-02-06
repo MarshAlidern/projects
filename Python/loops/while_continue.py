53c5c50134d3628660b6511b760df0e7071643f8
MarshAlidern <136602503+MarshAlidern@users.noreply.github.com>
2026-02-06 14:23:51 +0500

    we will skip it

---
+++ Python/loops/while_continue.py
@@ -0,0 +1,22 @@
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+i = 0
+while i < 10:
+    if i == 5:
+        i += 1
+        continue
+    print(i)
+    i += 1
+
+i = 0
+while i < 10:
+    if i == 5:
+        i += 1 
+        continue
+    print(i)
+    i += 1
