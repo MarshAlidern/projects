c518b039c5429d828708834b2f84fc2b2405b2fa
MarshAlidern <136602503+MarshAlidern@users.noreply.github.com>
2026-02-06 13:56:58 +0500

    boolean (logical) operators

---
+++ Python/boolean/boolean_operators.py
@@ -0,0 +1,16 @@
+x = 5
+print(x > 0 and x < 10)
+
+x = 5
+print(x < 5 or x > 10)
+
+x = 5
+print(not(x > 3 and x < 10))
+
+a = 10
+b = 12
+c = 0
+if a and b and c:
+    print("All the numbers have boolean value as True")
+else:
+    print("Atleast one number has boolean value as False")
