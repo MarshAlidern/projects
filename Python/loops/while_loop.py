a50fa5df31261a8f964929d099eb174409efeb3f
MarshAlidern <136602503+MarshAlidern@users.noreply.github.com>
2026-02-06 14:12:05 +0500

    while loops in general

---
+++ Python/loops/while_loop.py
@@ -0,0 +1,21 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+k=10
+while k!=2:
+  print(k*2)
+  k-=1
+
+count = 0
+while count < 3:
+    count = count + 1
+    print("Hello, World!")
+
+age = 28
+while age > 19:
+    print('Infinite Loop')
+
+while True:
+  print('It is always true')
