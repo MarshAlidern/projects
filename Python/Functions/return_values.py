60e3cb36f02bc3f06c48363f9ef5fc9110743b93
MarshAlider <alimuhan007@gmail.com>
2026-02-20 14:19:37 +0500

    returning

---
+++ Python/Functions/return_values.py
@@ -0,0 +1,30 @@
+def myfunction():
+  return 3+3
+
+print(myfunction()) 
+
+def myfunction():
+  return 3+3
+  print("Hello, World!")
+
+print(myfunction()) 
+
+def square(n):
+    return n * n
+
+res = square(4)
+print(res)
+
+def get_vals():
+    x = 10
+    y = 20
+    return x, y
+
+a, b = get_vals()
+print(a)
+print(b)
+
+def calc(n):
+    return [n * n, n * n * n]
+
+print(calc(3))
